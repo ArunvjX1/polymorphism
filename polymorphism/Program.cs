90ea1e32e555c95ca86a6656d4f5772f24c538fe
ARUN VIJAY <ARUN VIJAY@ARUNVJX1>
2022-11-10 20:23:39 +0000

    Add project files.

---
+++ polymorphism/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using polymorphism.com.polymorphism;
+namespace polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Polymorphism!");
+            check1 inherit = new inherit1();
+            inherit.show();
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ polymorphism/com/polymorphism/check.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism.com.polymorphism
+{
+
+    class inherit1 : check1
+    {
+        public void inherit2()
+        {
+
+            Console.WriteLine("not a base class");
+        }
+    }
+    
+}
